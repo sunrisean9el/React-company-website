5dcdf5fdf7a098d8948d06a7baec5894894cf0fe
sunrisean9el <kasztan7331@gmail.com>
2023-08-18 12:01:09 +0200

    Add mapping to create single ServiceBox

--- src/components/Offer/Offer.jsx
+++ src/components/Offer/Offer.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import services from "../../data/Services";
 import ServiceBox from "./ServiceBox";
 
 const Offer = () => {
   return (
-    <section id="offer" className="offer">
+    <section id="offer" className="section offer">
       <div className="offer-container container">
         <h2 className="offer-title">Czym zajmuje się nasza firma?</h2>
         <div className="service">
-          <ServiceBox />
+          {services.map((item) => (
+            <ServiceBox
+              key={item.id}
+              name={item.name}
+              number={item.id}
+              isNew={item.isNew}
+            />
+          ))}
         </div>
       </div>
     </section>

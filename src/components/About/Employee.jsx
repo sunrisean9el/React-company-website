c1fa10f0cc9d2737657297effd14c760b0e8a290
sunrisean9el <kasztan7331@gmail.com>
2023-08-18 12:04:09 +0200

    Pass correct prop

--- src/components/About/Employee.jsx
+++ src/components/About/Employee.jsx
@@ -1,28 +1,24 @@
-import React from "react";
-import employees from "../../data/Employees";
 import manPicture from "../../images/man.jpg";
 import womanPicture from "../../images/woman.jpg";
 
-const Employee = () => {
-  return employees.map((item, id) => {
-    return (
-      <div className="employee">
-        <div className="employee-photo-wrapper">
-          <img
-            src={id === 0 ? manPicture : womanPicture}
-            alt="Zdjęcie pracownika"
-            className="employee-photo"
-          />
-        </div>
-        <div className="employee-bio">
-          <p className="employee-name">
-            {item.firstName} {item.lastName} {item.position}
-          </p>
-          <p className="employee-description">{item.description}</p>
-        </div>
+const Employee = ({ id, firstName, lastName, position, description }) => {
+  return (
+    <div className="employee">
+      <div className="employee-photo-wrapper">
+        <img
+          src={id === 1 ? manPicture : womanPicture}
+          alt="Zdjęcie pracownika"
+          className="employee-photo"
+        />
       </div>
-    );
-  });
+      <div className="employee-bio">
+        <p className="employee-name p-tag">
+          {firstName} {lastName} {position}
+        </p>
+        <p className="employee-description p-tag">{description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Employee;

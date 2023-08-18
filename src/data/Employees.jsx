76fdf2c5e1905997a179b0861048172b2e6df670
sunrisean9el <kasztan7331@gmail.com>
2023-08-18 11:58:53 +0200

    Add employees id

--- src/data/Employees.jsx
+++ src/data/Employees.jsx
@@ -1,5 +1,6 @@
 const employees = [
   {
+    id: 1,
     firstName: "Imię",
     lastName: "Nazwisko",
     position: "[dział]",
@@ -7,6 +8,7 @@ const employees = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel massa et lacus egestas cursus a non magna. Fusce scelerisque blandit nunc, id malesuada ex lobortis a. Integer felis nisi, tempor elementum lorem in, varius pellentesque ligula. Duis efficitur lacinia enim, non tincidunt libero ultrices in.",
   },
   {
+    id: 2,
     firstName: "Imię",
     lastName: "Nazwisko",
     position: "[dział]",
